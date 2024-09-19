eca5861e0e2f0aa461ac9eac961b0387497cab2d
Your Name <you@example.com>
2024-09-20 01:24:57 +0600

    transaction history part done

--- js/home.js
+++ js/home.js
@@ -1,14 +1,4 @@
-/*
-btn-add-money
-input-pin
-input-amount
-add-money
 
-out-money
-output-amount
-output-pin
-btn-out-money
-*/
 
 // btns 
 const balanceText = document.getElementById('balance').innerText;
@@ -32,6 +22,8 @@ document.getElementById('add-money-btn').addEventListener('click', function () {
     outMoneyDiv.classList.add('hidden');
     divider.classList.add('bg-green-500');
     divider.classList.remove('bg-orange-500');
+    divider.classList.remove('bg-slate-500');
+    document.getElementById('history-part').classList.add('hidden')
 })
 document.getElementById('out-money-btn').addEventListener('click', function () {
     addMoneyDiv.classList.add('hidden');
@@ -39,28 +31,50 @@ document.getElementById('out-money-btn').addEventListener('click', function () {
     demoDiv.classList.add('hidden');
     divider.classList.add('bg-orange-500');
     divider.classList.remove('bg-green-500');
+    divider.classList.remove('bg-slate-500');
+    document.getElementById('history-part').classList.add('hidden')
+})
+document.getElementById('history').addEventListener('click', function () {
+    addMoneyDiv.classList.add('hidden');
+    outMoneyDiv.classList.add('hidden');
+    demoDiv.classList.add('hidden');
+    document.getElementById('history-part').classList.remove('hidden')
+    divider.classList.add('bg-slate-500');
+    divider.classList.remove('bg-green-500');
+    divider.classList.remove('bg-orange-500');
 })
 
 document.getElementById('logo').addEventListener('click', function () {
     addMoneyDiv.classList.add('hidden');
     outMoneyDiv.classList.add('hidden');
     demoDiv.classList.remove('hidden');
-
+    document.getElementById('history-part').classList.add('hidden')
 })
 
 // calculations 
-
-// inPin inMoney 
+const history = document.getElementById('history-box');
 
 addBtn.addEventListener('click', function (event) {
     event.preventDefault();
     const inPin = document.getElementById('input-pin').value;
     let inMoney = document.getElementById('input-amount').value;
     inMoney = Number(inMoney);
+    if(isNaN(inPin)){
+        alert('Pin only consists of numbers');
+    }
     if (inPin === '017') {
         let newBalance = balance + inMoney;
         document.getElementById('balance').innerText = newBalance;
-        document.getElementById('container').style.opacity = '100%';
+
+        // history management
+
+        const p = document.createElement('p')
+        p.innerHTML = `
+             Money added ${inMoney} , total : ${newBalance}
+        `
+        p.classList.add("text-slate-400", "bg-lime-200");
+        history.append(p);
+
     }
     else {
         window.location.href = './index.html';
@@ -73,10 +87,20 @@ outBtn.addEventListener('click', function (event) {
     const outPin = document.getElementById('output-pin').value;
     let outMoney = document.getElementById('output-amount').value;
     outMoney = Number(outMoney);
+    if(isNaN(outPin)){
+        alert('Pin only consists of numbers');
+    }
     if (outPin === '017') {
         let newBalance = balance - outMoney;
         document.getElementById('balance').innerText = newBalance;
-        document.getElementById('container').style.opacity = '100%';
+        // history management
+
+        const p = document.createElement('p')
+        p.innerHTML = `
+                     Money out ${outMoney} , total : ${newBalance}
+                `
+        p.classList.add("text-slate-400", "bg-yellow-200");
+        history.append(p);
     }
     else {
         window.location.href = './index.html';
